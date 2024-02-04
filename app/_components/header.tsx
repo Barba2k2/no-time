cc9d712b7d627485bdc5ab01e0137d283369645a
Barba2k2 <lorenzo.dz@hotmail.com>
2024-02-04 09:02:18 -0300

    feta: created header component

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,12 +1,46 @@
+import Link from "next/link";
 import { Card, CardContent } from "./ui/card";
+import Image from "next/image";
+import { Button } from "./ui/button";
 
 const Header = () => {
   return (
-    <Card>
-      <CardContent>
-        <h1>Este e o meu header</h1>
-      </CardContent>
-    </Card>
+    <header>
+      <Card className="border-none shadow-none px-2 py-3">
+        <CardContent className="bg-[#EDEDED] rounded-full h-[80px] z-99">
+          <div className="flex flex-row justify-between items-center ">
+            <Link href="/">
+              <Image
+                src="/header-logo.png"
+                width={370}
+                height={64}
+                alt="No Time Fast Services"
+                className="cursor-pointer mt-1"
+              />
+            </Link>
+            <div className="flex flex-row">
+              <Link href="/" className="px-2">
+                <p className="text-base">Inicio</p>
+              </Link>
+              <Link href="/" className="px-2">
+                <p className="text-base">Encontre trabalho</p>
+              </Link>
+              <Link href="/" className="px-2">
+                <p className="text-base">Entre</p>
+              </Link>
+              <Link href="/" className="px-2">
+                <p className="text-base">Cadastre-se</p>
+              </Link>
+            </div>
+            <div className="">
+              <Button className="relative py-8 uppercase font-bold text-white text-xl bg-[#10BFCC] rounded-full">
+                Publicar Projeto
+              </Button>
+            </div>
+          </div>
+        </CardContent>
+      </Card>
+    </header>
   );
 };
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,10 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 
-const poppins = Poppins({ subsets: ["latin"], weight: ["400", "700"] });
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 export const metadata: Metadata = {
   title: "No Time - Fast Services",
